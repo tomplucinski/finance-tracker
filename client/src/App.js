b780710bc48049daa0bce3bc7ead8b6ecb457eae
Tom Plucinski <tapluci1@gmail.com>
2021-07-03 13:51:37 -0700

    Bootstrapped victory charts in UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
+
+  const mockData = [
+    {quarter: 1, earnings: 13000},
+    {quarter: 2, earnings: 16500},
+    {quarter: 3, earnings: 14250},
+    {quarter: 4, earnings: 19000}
+  ];
 
   useEffect(() => {
     fetch("/api")
@@ -16,6 +25,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Finance Tracker</h1>
+        <VictoryChart
+          domainPadding={20}
+        >
+          <VictoryAxis
+          // tickValues specifies both the number of ticks and where
+          // they are placed on the axis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          />
+          <VictoryAxis
+            dependentAxis
+            // tickFormat specifies how ticks should be displayed
+            tickFormat={(x) => (`$${x / 1000}k`)}
+          />
+          <VictoryBar
+            data={mockData}
+            x="quarter"
+            y="earnings"
+          />
+        </VictoryChart>
         <p>{!data ? 'Loading...' : data}</p>
       </header>
     </div>
