29546f35c88afa12f7ec1c23b60a798a2eae79f3
Tom Plucinski <tapluci1@gmail.com>
2021-07-05 13:55:21 -0700

    added local dev script

--- client/src/App.js
+++ client/src/App.js
@@ -29,14 +29,11 @@ function App() {
           domainPadding={20}
         >
           <VictoryAxis
-          // tickValues specifies both the number of ticks and where
-          // they are placed on the axis
           tickValues={[1, 2, 3, 4]}
           tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
           />
           <VictoryAxis
             dependentAxis
-            // tickFormat specifies how ticks should be displayed
             tickFormat={(x) => (`$${x / 1000}k`)}
           />
           <VictoryBar
