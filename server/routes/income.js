02c8b324c9767c94509ade07317ff3e35518e57e
Tom Plucinski <tapluci1@gmail.com>
2021-07-19 21:06:19 -0700

    pulling income data from api

--- server/routes/income.js
+++ server/routes/income.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const router = express.Router();
-const Income = require('../models/Income');
+const express = require('express')
+const router = express.Router()
+const Income = require('../models/Income')
 
 // @route   GET api/income
 // @desc    Get all income
 router.get('/', async (req, res) => {
     try {
-        res.json('Here is all your tracked income!');
+        const income = await Income.find()
+        if (!income.length) return res.status(404).json({ message: 'No income found' })
+        res.json(income)
     } catch(e) {
-        console.error(e);
-        res.status(500).send('Server error');
+        console.error(e)
+        res.status(500).json({ message: 'Server error' })
     }
-});
+})
 
 // @route   POST api/income
 // @desc    Add income
@@ -22,9 +24,9 @@ router.post('/', async (req, res) => {
         const income = await newIncome.save()
         res.json(income)
     } catch (e) {
-        console.error(e);
+        console.error(e)
         res.status(500).send('Server error')
     }
-});
+})
 
-module.exports = router;
+module.exports = router
